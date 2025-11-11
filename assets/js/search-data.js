2a4367150c14b088452e83f43bf3624d93b31a2b
Arshdeep-Singh-Boparai <59464293+Arshdeep-Singh-Boparai@users.noreply.github.com>
2025-11-11 17:44:08 +0000

    Deploying to gh-pages from @ Arshdeep-Singh-Boparai/Arshdeep-Singh-Boparai.github.io@ab25fbc4199f52c3d8fae9b1af009a707b5b07d5 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -428,9 +428,9 @@ ninja.data = [{
           description: "Research Fellow in Machine Learning for Sound",
           section: "Projects",handler: () => {
               window.location.href = "/projects/1_project/";
-            },},{id: "projects-project-2",
-          title: 'project 2',
-          description: "a project with a background image and giscus comments",
+            },},{id: "projects-sound-wellbeing-in-later-life",
+          title: 'Sound wellbeing in later life',
+          description: "Exploring wellbeing through contextual understanding of sound and sonic reminiscence.",
           section: "Projects",handler: () => {
               window.location.href = "/projects/2_project/";
             },},{id: "projects-project-3-with-very-long-name",
