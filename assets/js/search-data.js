37fde3bd4583b564d6519425e09a2a2730048968
Arshdeep-Singh-Boparai <59464293+Arshdeep-Singh-Boparai@users.noreply.github.com>
2025-11-11 17:37:08 +0000

    Deploying to gh-pages from @ Arshdeep-Singh-Boparai/Arshdeep-Singh-Boparai.github.io@0e6419822a90bc11338050b23a859511b80b25a4 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -423,9 +423,9 @@ ninja.data = [{
             },},{id: "news-a-simple-inline-announcement-with-markdown-emoji-sparkles-smile",
           title: 'A simple inline announcement with Markdown emoji! :sparkles: :smile:',
           description: "",
-          section: "News",},{id: "projects-project-1",
-          title: 'project 1',
-          description: "with background image",
+          section: "News",},{id: "projects-ai-for-sound",
+          title: 'AI for Sound',
+          description: "Research Fellow in Machine Learning for Sound",
           section: "Projects",handler: () => {
               window.location.href = "/projects/1_project/";
             },},{id: "projects-project-2",
