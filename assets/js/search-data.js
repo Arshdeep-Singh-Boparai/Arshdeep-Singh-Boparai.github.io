bb48c9225ae794d4d12599ef3808603defdb8ef8
Arshdeep-Singh-Boparai <59464293+Arshdeep-Singh-Boparai@users.noreply.github.com>
2025-11-15 18:06:09 +0000

    Deploying to gh-pages from @ Arshdeep-Singh-Boparai/Arshdeep-Singh-Boparai.github.io@f80bc04eb646e7c16923abfa34eb2d06d3be6f25 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -433,9 +433,9 @@ ninja.data = [{
           description: "Research Associate",
           section: "Projects",handler: () => {
               window.location.href = "/projects/2_project/";
-            },},{id: "projects-project-3-with-very-long-name",
-          title: 'project 3 with very long name',
-          description: "a project that redirects to another website",
+            },},{id: "projects-omega-2-3-project",
+          title: 'OMEGA-2.3 project',
+          description: "Project Fellow",
           section: "Projects",handler: () => {
               window.location.href = "/projects/3_project/";
             },},{id: "projects-project-4",
